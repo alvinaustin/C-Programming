6d90db0cc717324993f411101e51424512336562
alvinaustin <alvinjosephaustin2028@cs.sjcetpalai.ac.in>
2025-02-05 15:19:46 +0530

    largestof3

---
+++ largestof3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){ 
+int num1,num2,num3;
+printf("Enter 3 integers");
+scanf("%d%d%d",&num1,&num2,&num3);
+if (num1>num2){
+  if (num1>num2){
+  printf("%d is the greatest number",num1);
+  }
+}
+else if (num2>num3){
+ printf("%d is the greatest number",num2);
+ }
+ else{
+  printf("%d is the greatest number",num3);
+  
+}
+
+return 0;
+
+
+
+
+
+
+
+}
